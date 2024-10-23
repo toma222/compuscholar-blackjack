14f6935da2fb1870c0d4eff8b822ffe822b27e08
basilly <aidannwilhite@gmail.com>
2024-10-23 14:53:34 -0500

    ported the assignment for 1

---
+++ lab-1/assignment/BlackJackCard.java
@@ -0,0 +1,13 @@
+public class BlackJackCard extends Card
+{
+    //write constructors here
+
+
+    public int getValue()
+    {
+        //override the Card getValue to build BlackJack logic
+        //enables you to build the value for the game into the card
+        //this makes writing the whole program a little
+        easier return 0;
+    }
+}
