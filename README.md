def10b1352856fe9f66f8d1f50cbd3399096745a
basilly <aidannwilhite@gmail.com>
2024-10-24 13:07:49 -0500

    updated read me

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 This is meant to be a porting of the compu-scholar blackjack assignment to
 a "Mr.Deere" sort of github assignment
 
+how this should work is that for each "lab" there are 2 files: assignment and answers.
+The folder that says assignment should have a readme with a Mr.Deere-equ type assignment.
+I would recommend doing the assignment folder instead of just copying the code from the answer folder.
+
+But of unfortunately you have free will.
---
+++ lab-2/assingment/Deck.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck
+{
+  public static final int NUMFACES = 13; 
+  public static final int NUMSUITS = 4; 
+  public static final int NUMCARDS = 52;
+  public static final String SUITS[] = {"CLUBS","SPADES","DIAMONDS","HEARTS"}; 
+  private int topCardIndex; // index of the top most card in the deck
+  private ArrayList<Card> stackOfCards;
+
+  //constructor 
+  public Deck()
+  {
+    //initialize the instance variables
+    topCardIndex = 52;
+    stackOfCards = new ArrayList<BlackJackCard>();
+
+    //one loop to go through all SUITS
+      //one loop to go through all FACES
+      //add in each new BlackJackCard() to the Deck
+      
+    for(int i = 0; i < NUMSUITS; i++)
+        for(int j = 0; j < NUMFACES; j++)
+            stackOfCards.add(new BlackJackCard(SUITS[i], j));
+  }
+
+  // number of cards left in the deck
+  public int numCardsLeft()
+  {
+    return topCardIndex;
+  }
+
+  public void shuffle()
+  {
+    //add code to shuffle deck – Collections has a shuffle
+    // use the package - import java.util.Collections;
+    Collections.shuffle(stackOfCards);
+    topCardIndex = 52; // reset the top of the deck
+  }
+
+  public Card nextCard()
+  {
+    return stackOfCards.get(--topCardIndex); 
+  } 
+
+  public String toString()
+  {
+    return stackOfCards + "	topCardIndex = " + topCardIndex;
+  }
+}
